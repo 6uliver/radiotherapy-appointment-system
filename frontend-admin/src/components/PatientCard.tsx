25a31c0234c4f7653d9afd046e0f9cf01de52b96
Németh-Szegedi Judit <nemethszegedi.judit@gmail.com>
2023-11-25 17:50:38 +0100

    add react icons

--- frontend-admin/src/components/PatientCard.tsx
+++ frontend-admin/src/components/PatientCard.tsx
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+import { GiFat } from "react-icons/gi";
+import { FaLungs, FaBed} from "react-icons/fa";
+import { LuImagePlus } from "react-icons/lu";
+
+const Container = styled.div`
+
+`;
+
 export function PatientCard(){
-    return <div>PatientCard</div>
+    <Container>
+    return <div>PatientCard <div>13/15</div><GiFat /><FaLungs /><FaBed /><LuImagePlus />hh
+</div>
+    </Container>
 }
