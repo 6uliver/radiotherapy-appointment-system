ea6895bba6393ca38ab477ce2bae74a0b7dac9e3
Németh-Szegedi Judit <nemethszegedi.judit@gmail.com>
2023-11-25 23:07:05 +0100

    improve patient datas

--- frontend-admin/src/components/TreatmentPlan.tsx
+++ frontend-admin/src/components/TreatmentPlan.tsx
@@ -1,47 +1,91 @@
 import styled from "styled-components";
+import { OncoDarkGreen } from "../theme";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-const Box = styled.div`
-  border-bottom: 2px black solid;
-  border-top: 2px black solid;
+const Section = styled.div`
+  border: 2px solid ${OncoDarkGreen};
+  border-radius: 10px;
+  padding: 20px;
 
-  &:first-child {
-    background-color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Parameter = styled.div`
+  display: flex;
+
+  :last-child {
+    font-weight: bold;
+    padding: 0 30px;
+  }
+`;
+
+const Name = styled.div`
+  display: flex;
+  font-weight: bold;
+  font-size: 30px;
+
+  :last-child {
+    font-weight: bold;
   }
 `;
 
 export function TreatmentPlan() {
   return (
     <Container>
-      <section>
-        <Box>
-          <ul>
-            <li>Birth name</li>
-            <li>Date of birth</li>
-            <li>TAJ</li>
-            <li>E-mail</li>
-            <li>Phone</li>
-          </ul>
-        </Box>
-        <Box>
-          <ul>
-            <li>Region</li>
-            <li>Fractions</li>
-            <li>Inpatient</li>
-            <li>Mobility</li>
-            <li>Need assistance for transport</li>
-            <li>Large-bodied</li>
-            <li>Breath-holding</li>
-            <li>kV imaging</li>
-          </ul>
-        </Box>
+      <Section>
+        <h1>Patient</h1>
+        <Name>Molnár Lajosné</Name>
+        <Parameter>
+          Birth name <div>Kis Mária</div>
+        </Parameter>
+        <Parameter>
+          Date of birth<div>1963.08.02.</div>
+        </Parameter>
+        <Parameter>
+          SSN<div>123 456 789</div>
+        </Parameter>
+        <Parameter>
+          Email<div>kismaria@example.com</div>
+        </Parameter>
+        <Parameter>
+          Phone<div>+36701234567</div>
+        </Parameter>
+        <Parameter>
+          Region <div>breast</div>
+        </Parameter>
+        <Parameter>
+          Fractions <div>1/6</div>
+        </Parameter>
+        <Parameter>
+          Inpatient<div>yes</div>
+        </Parameter>
+        <Parameter>
+          Mobility<div>wheelchair</div>
+        </Parameter>
+        <Parameter>
+          Need assistance for transport<div>yes</div>
+        </Parameter>
+        <Parameter>
+          Large-bodied<div>yes</div>
+        </Parameter>
+        <Parameter>
+          Breath-holding<div>yes</div>
+        </Parameter>
+        <Parameter>
+          kV imaging<div>yes</div>
+        </Parameter>
         <div>Comments</div>
-      </section>
-      <section>
-        <h1>Schedule - 1/6</h1>
+      </Section>
+      <Section>
+        <h1>Schedule</h1>
         <button>edit</button>
-      </section>
+      </Section>
     </Container>
   );
 }
