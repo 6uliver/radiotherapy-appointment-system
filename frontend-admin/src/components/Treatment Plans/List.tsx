5fde66096cc9df39e49e02b12fdffa08dc4aea08
Roland Németh <info@nemethroland.hu>
2023-11-25 21:25:53 +0100

    Show search results

--- frontend-admin/src/components/Treatment Plans/List.tsx
+++ frontend-admin/src/components/Treatment Plans/List.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FragmentType, gql, useFragment } from "../../gql";
 
 const Container = styled.div`
   margin-top: 15px;
@@ -6,10 +7,36 @@ const Container = styled.div`
   background-color: white;
 `;
 
-export function List() {
+const fragment = gql(/* GraphQL */ `
+  fragment TreatmentPlanForList on TreatmentPlan {
+    id
+    fractionCount
+    fractionMinutes
+    patient {
+      id
+      firstName
+      lastName
+    }
+  }
+`);
+
+interface Props {
+  treatmentPlans: FragmentType<typeof fragment>[];
+}
+
+export function List({ treatmentPlans }: Props) {
+  const treatmentPlansFragment = useFragment(fragment, treatmentPlans);
   return (
     <Container>
       <h1>Results</h1>
+      <div>
+        {treatmentPlansFragment.map((treatmentPlan) => (
+          <div key={treatmentPlan.id}>
+            {treatmentPlan.id} - {treatmentPlan.fractionMinutes} -{" "}
+            {treatmentPlan.patient.firstName} {treatmentPlan.patient.lastName}
+          </div>
+        ))}
+      </div>
     </Container>
   );
 }
--- frontend-admin/src/components/Treatment Plans/TreatmentPlans.tsx
+++ frontend-admin/src/components/Treatment Plans/TreatmentPlans.tsx
@@ -2,14 +2,33 @@ import { useState } from "react";
 import { PageBase } from "../PageBase";
 import { List } from "./List";
 import { Search } from "./SearchBar";
+import { gql } from "../../gql";
+import { useQuery } from "@apollo/client";
+
+const search = gql(/* GraphQL */ `
+  query searchTreatmentPlans($searchTerm: String!) {
+    searchTreatmentPlans(searchTerm: $searchTerm) {
+      ...TreatmentPlanForList
+    }
+  }
+`);
 
 export function TreatmentPlans() {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const searchQuery = useQuery(search, {
+    variables: { searchTerm },
+    skip: !searchTerm,
+  });
+
+  console.log(searchQuery.data);
+
   return (
     <PageBase title="Treatment Plans">
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      {searchTerm && <List />}
+      {searchQuery.data && (
+        <List treatmentPlans={searchQuery.data.searchTreatmentPlans} />
+      )}
     </PageBase>
   );
 }
