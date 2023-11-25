67e6f9c99be36ab27fa485432fd1f1926b6c36a9
Roland Németh <info@nemethroland.hu>
2023-11-25 01:03:50 +0100

    Improve Apollo config

--- frontend-admin/codegen.ts
+++ frontend-admin/codegen.ts
@@ -2,8 +2,27 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "src/schema.graphql",
-  generates: {},
+  schema: "./schema.gql",
+  generates: {
+    "./src/gql/": {
+      documents: ["!src/gql/**/*", "src/**/*.{ts,tsx}"],
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+      plugins: [],
+      config: {
+        avoidOptionals: {
+          field: true,
+          inputValue: false,
+          object: false,
+          defaultValue: true,
+        },
+        inputMaybeValue: "T | undefined",
+        enumsAsTypes: true,
+      },
+    },
+  },
 };
 
 export default config;
--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,3 +1,18 @@
+import { UserList } from "./UserList";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 export function App() {
-  return <h1>OncoSync</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h1>OncoSync</h1>
+        <UserList />
+      </div>
+    </ApolloProvider>
+  );
 }
