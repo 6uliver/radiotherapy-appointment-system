d7d8ff668a95747843e9032925bb96022f879e05
Németh-Szegedi Judit <nemethszegedi.judit@gmail.com>
2023-11-25 20:39:15 +0100

    improve patient card and treatment

--- frontend-admin/src/components/PatientCard.tsx
+++ frontend-admin/src/components/PatientCard.tsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 import { GiFat } from "react-icons/gi";
-import { FaLungs, FaBed} from "react-icons/fa";
+import { FaLungs, FaBed } from "react-icons/fa";
 import { BiSolidImageAdd } from "react-icons/bi";
 
-const Container = styled.div`
+const Container = styled.div``;
 
-`;
-
-export function PatientCard(){
-    return <Container>
-    <div>Molnár Lajosné <div>13/15</div><GiFat /><FaLungs /><FaBed /><BiSolidImageAdd /></div>
+export function PatientCard() {
+  return (
+    <Container>
+      <div>
+        Molnár Lajosné
+        <div>breast</div>
+        <div>13/15</div>
+        <GiFat />
+        <FaLungs />
+        <FaBed />
+        <BiSolidImageAdd />
+      </div>
     </Container>
+  );
 }
--- frontend-admin/src/components/TreatmentPlan.tsx
+++ frontend-admin/src/components/TreatmentPlan.tsx
@@ -1,28 +1,47 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Box = styled.div`
+  border-bottom: 2px black solid;
+  border-top: 2px black solid;
+
+  &:first-child {
+    background-color: aliceblue;
+  }
+`;
+
 export function TreatmentPlan() {
   return (
-    <section>
-      <div>
-        <ul>
-          <li>Birth name</li>
-          <li>Date of birth</li>
-          <li>TAJ</li>
-          <li>E-mail</li>
-          <li>Phone</li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Region</li>
-          <li>Fractions</li>
-          <li>Inpatient</li>
-          <li>Mobility</li>
-          <li>Need assistance for transport</li>
-          <li>Large-bodied</li>
-          <li>Breath-holding</li>
-          <li>kV imaging</li>
-        </ul>
-      </div>
-      <div>Comments</div>
-    </section>
+    <Container>
+      <section>
+        <Box>
+          <ul>
+            <li>Birth name</li>
+            <li>Date of birth</li>
+            <li>TAJ</li>
+            <li>E-mail</li>
+            <li>Phone</li>
+          </ul>
+        </Box>
+        <Box>
+          <ul>
+            <li>Region</li>
+            <li>Fractions</li>
+            <li>Inpatient</li>
+            <li>Mobility</li>
+            <li>Need assistance for transport</li>
+            <li>Large-bodied</li>
+            <li>Breath-holding</li>
+            <li>kV imaging</li>
+          </ul>
+        </Box>
+        <div>Comments</div>
+      </section>
+      <section>
+        <h1>Schedule - 1/6</h1>
+        <button>edit</button>
+      </section>
+    </Container>
   );
 }
