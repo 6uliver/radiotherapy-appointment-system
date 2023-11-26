3d314c9bd5cab30a12a86d6406c8c4ec14f342af
Destaran <abonyibalazs95@gmail.com>
2023-11-26 04:23:57 +0100

    improved MachinePage

--- frontend-admin/src/components/Machines/MachinePage.tsx
+++ frontend-admin/src/components/Machines/MachinePage.tsx
@@ -1,17 +1,35 @@
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { PageBase } from "../PageBase";
+import { gql } from "../../gql";
+import { useQuery } from "@apollo/client";
 
 const Container = styled.div``;
+
+const machine = gql(`
+  query Machine($id: ID!) {
+    machineById(id: $id) {
+      id
+      name
+      }
+    }
+`);
 
 export function MachinePage() {
   const { id } = useParams<"id">();
 
+  const machineQuery = useQuery(machine, {
+    variables: { id: id ?? "" },
+    skip: !id,
+  });
+
+  if (!machineQuery.data) {
+    return;
+  }
+
   return (
-    <PageBase title="machine-name">
-      <Container>
-        <h1>im machinepage - {id}</h1>
-      </Container>
+    <PageBase title={machineQuery.data.machineById.name}>
+      <Container></Container>
     </PageBase>
   );
 }
